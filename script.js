2d1bf93817350713ddc30ff045fe2b022e5a8080
Trisha Rajeev <trisharajeevv@gmail.com>
2026-09-16 20:57:15 +0530

    Add deleteTask function and click handler

--- script.js
+++ script.js
@@ -3,4 +3,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const taskList = document.getElementById("taskList");
   console.log("To-Do app loaded");
+
+  function deleteTask(taskItem) {
+    taskItem.remove();
+  }
+
+  taskList.addEventListener("click", function (e) {
+    if (e.target.tagName === "BUTTON") {
+      deleteTask(e.target.parentElement);
+    }
+  });
 });
