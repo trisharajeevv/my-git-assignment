5e45c24d48638528898c3b4dea693260d9b4a832
Trisha Rajeev <trisharajeevv@gmail.com>
2026-09-16 20:56:08 +0530

    Add script.js with basic setup

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js - To-Do List logic
+
+document.addEventListener("DOMContentLoaded", function () {
+  const taskList = document.getElementById("taskList");
+  console.log("To-Do app loaded");
+});
